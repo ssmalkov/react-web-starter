83fa80b7f9271b30ce3b3f98e60504c1d703cc3c
Sergei Smalkov <ssmalkov@gmail.com>
2018-07-10 17:41:57 +0500

    initial

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
 
 export class Todo extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      nums: [1, 2, 3, 4, 5]
+    }
+  }
+  
+  _handleClick(){
+    const newNums = [...this.state.nums, 6];
+    this.setState({nums: newNums})
+  }
+
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <input type="text" />
+        <button onClick={this._handleClick.bind(this)}>click</button>
+        {this.state.nums.map(num => (<h3 key={num}>{num}</h3>))}
+      </div>
     );
   }
+  
 }
