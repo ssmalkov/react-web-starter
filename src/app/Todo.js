7e7dec9a26721e3c8d2edfa316506e96265a4119
Sergei Smalkov <ssmalkov@gmail.com>
2018-07-10 19:17:30 +0500

    todo solution implemented fixed todo remove method

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -5,23 +5,49 @@ export class Todo extends Component {
   constructor(){
     super();
     this.state = {
-      nums: [1, 2, 3, 4, 5]
+      todos: [],
+      newTodo: '',
     }
   }
   
-  _handleClick(){
-    const newNums = [...this.state.nums, 6];
-    this.setState({nums: newNums})
+  _handleChange(e){
+    const {value} = e.target;
+    this.setState({newTodo: value})
+  }
+
+  _handleClick(e){
+    e.preventDefault();
+    const todos = [...this.state.todos, this.state.newTodo];
+    this.setState({todos, newTodo: ''});
+  }
+
+  _removeTodo(index){
+    const todos = this.state.todos;
+    todos.splice(index, 1);
+    this.setState({todos});
   }
 
   render() {
     return (
+
       <div>
-        <input type="text" />
-        <button onClick={this._handleClick.bind(this)}>click</button>
-        {this.state.nums.map(num => (<h3 key={num}>{num}</h3>))}
+        <form>
+          <input onChange={this._handleChange.bind(this)} value={this.state.newTodo} type="text" placeholder="new todo" />
+          <button onClick={this._handleClick.bind(this)}>Create todo</button>
+        </form>
+        <ul>
+          {this.state.todos.map((todo, index) => (
+              <li onClick={() => this._removeTodo.call(this, index)} key={index}>{todo}</li>
+          ))}
+        </ul>
       </div>
+
+      // <div>
+      //   <input type="text" />
+      //   <button onClick={this._handleClick.bind(this)}>click</button>
+      //   {this.state.nums.map(num => (<h3 key={num}>{num}</h3>))}
+      // </div>
     );
   }
-  
+
 }
